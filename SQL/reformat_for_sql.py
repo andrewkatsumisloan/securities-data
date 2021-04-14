45d2a8f87340ce981ce8bbb213bc7db532b2c3d2
andrewksloan <andrewksloan3@gmail.com>
2021-04-13 21:01:05 -0700

    Add Analysis, more reorganization, path fixes, lib, etc.

---
+++ SQL/reformat_for_sql.py
@@ -0,0 +1,22 @@
+import re
+
+from lib import JOINED_SP500_PATH
+
+handle = open(JOINED_SP500_PATH, 'r')
+lines = handle.readlines()
+handle.close()
+
+handle = open('sql_format.csv', 'w')
+handle.write('ticker,date_time,closing_price\n')
+
+tickers = re.split(',', lines[0].rstrip())[2:]
+
+for line in lines[1:]:
+    larray = re.split(',', line.rstrip())
+    date = larray[1]
+    prices = larray[2:]
+    for i in range(len(prices)):
+        if prices[i]:
+            handle.write('{0},{1},{2}\n'.format(tickers[i], date, prices[i]))
+
+handle.close()
--- df_test.py
+++ df_test.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import numpy as np
 
-with open('sp500_joined_closes.csv', 'r') as f:
+from lib import JOINED_SP500_PATH
+with open(JOINED_SP500_PATH, 'r') as f:
     joined_df = pd.read_csv(f, index_col=0)
     # joined_df = pd.read_csv(f, index_col=1)
 
@@ -18,17 +19,14 @@ with open('sp500_joined_closes.csv', 'r') as f:
 joined_df = joined_df.head(10)
 print(joined_df)
 
-
 # Iterate over every cell.
 for index, series in joined_df.iterrows():
     for column in joined_df.columns[1:]:
         joined_df.at[index, column] = 5
 
-
 for column, series in joined_df.iteritems():
     for a, index in enumerate(series):
         joined_df.at[a, column] = 5
-
 
 print('Modified', joined_df)
 
@@ -42,8 +40,8 @@ print('This is loc', joined_df.loc[2])
 # print(joined_df.loc(joined_df['AAPL']))
 
 # Describe shows interesting features of
-# print(joined_df.describe())
-# describe_df = (joined_df.describe()).to_csv('describe.csv')
+print(joined_df.describe())
+describe_df = (joined_df.describe()).to_csv('describe.csv')
 
 
 # Get row by index number
