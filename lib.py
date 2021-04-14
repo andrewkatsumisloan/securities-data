bd7bd174d496aee2427ef44c738e0e7292fddc05
andrewksloan <andrewksloan3@gmail.com>
2021-04-13 20:52:06 -0700

    Fixing paths, re-organizing code.

--- lib.py
+++ lib.py
@@ -1,5 +1,10 @@
 import os
 
-JOINED_SP500_PATH = os.path.abspath(os.path.join('sp500_joined_closes.csv'))
+CODE_DIR = os.path.dirname(__file__)
 
+JOINED_SP500_PATH = os.path.abspath(os.path.join(CODE_DIR, 'SP500_DATA', 'AggregateData', 'SP500_JOINED_CLOSES.CSV'))
 print(JOINED_SP500_PATH)
+
+PICKLE_PATH = os.path.abspath(os.path.join(CODE_DIR, 'sp500tickers.pickle'))
+
+JSON_DATA_PATH = os.path.abspath(os.path.join(CODE_DIR, 'data.json'))
