404079a43df672cc5d635b59d34d7b2d26a356a8
andrewksloan <andrewksloan3@gmail.com>
2022-01-31 08:12:49 -0800

    Added code for handling requests from frontend, given ticker, return data.

--- backend-securities/app.py
+++ backend-securities/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import pandas as pd
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def index():
@@ -14,12 +16,20 @@ def about():
 @app.route('/ticker_entry', methods=['GET', 'POST'])
 def get_ticker_data():
     if request.method == 'POST':  
-        ticker = request.args.get('ticker')
-        df = pd.read_csv('../raw_data/individual/{0}.csv'.format(ticker))
-        df = df.to_json(orient='records')
-        return jsonify(df)
-    else: 
-        return
+        ticker = request.get_json(['ticker'])
+        ticker = ticker['ticker']
+        print('This endpoint hit')
+        df = pd.read_csv('./sp_joined_closes.csv')
+        print(df)
+        # ticker = ticker.tostring()
+        ret_val = df[ticker]
+        ret_val = ret_val.to_json(orient='records')
+        print(ret_val)
+        return ret_val
+
+    if request.method == 'GET':
+        print('This endpoint hit')
+        return 'This endpoint hit'
 
 if __name__ == '__main__':
     app.run(debug=True)
