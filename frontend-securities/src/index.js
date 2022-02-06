fc0d6ebc701ca695f36deaeecfa9e03914646f2d
andrewksloan <andrewksloan3@gmail.com>
2022-02-06 14:28:43 -0800

    removed all auth0 for problematic dependencies.

--- frontend-securities/src/index.js
+++ frontend-securities/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
-import { Auth0Provider } from '@auth0/auth0-react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './index.css';
